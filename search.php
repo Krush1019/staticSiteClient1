71fc0b1dd6340650d278579d93b6314fa34f6d1b
Krushang <krushangprakashbhai1910@gmail.com>
2025-01-10 11:09:51 +0530

    initial commit for host page trial

---
+++ search.php
@@ -0,0 +1,200 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  	<title>Search results</title>
+  	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"> 
+    <meta name="description" content="Your description">
+    <meta name="keywords" content="Your keywords">
+    <meta name="author" content="Your name">
+
+    <link rel="icon" href="images/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" />
+
+    <link rel="stylesheet" href="css/reset.css" type="text/css" media="all">
+    <!-- <link rel="stylesheet" href="css/fonts.css" type="text/css" media="all"> -->
+    <link rel="stylesheet" href="css/style.css" type="text/css" media="all">
+
+
+    <script src="js/jquery.js"></script>
+	<script src="search/search.js"></script>
+     <script src="js/jquery-migrate-1.1.1.js"></script>
+     <script src="js/superfish.js"></script>
+     <script src="js/jquery.mobilemenu.js"></script>
+
+
+     <script src="js/jquery.easing.1.3.js"></script>
+     <script src="js/scroll_to_top.js"></script>
+     <script src="js/script.js"></script>
+     <script src="js/sForm.js"></script>
+     <script src="js/jquery.equalheights.js"></script>
+     <script src="js/touchTouch.jquery.js"></script>
+
+     <!-- font-awesome font -->
+     <link rel="stylesheet" href="font/font-awesome.css" type="text/css" media="screen">
+     <!-- fontello font -->
+
+<!--[if lt IE 8]>
+   <div style=' clear: both; text-align:center; position: relative;'>
+     <a href="http://windows.microsoft.com/en-US/internet-explorer/products/ie/home?ocid=ie6_countdown_bannercode">
+       <img src="http://storage.ie6countdown.com/assets/100/images/banners/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today." />
+    </a>
+  </div>
+<![endif]-->
+<!--[if lt IE 9]>
+	<script src="js/html5.js"></script>
+	<link rel="stylesheet" href="css/ie.css"> 
+<![endif]-->
+</head>
+<body>
+<!--button back top-->
+<div id="back-top"></div>
+  <div class="main">
+
+    <!--==============================header=================================-->
+        <header>
+            <div class="container_12">
+                <div class="grid_12">
+                    <div class="top_section">
+                        <p>24/7 Support:<br><strong class="icon-phone"></strong> +1 959 603 6035<span class="vline1"></span> <strong class="icon-envelope-alt"></strong> <a href="#">mail@demolink.org</a></p>
+                    </div>
+                    
+                    <h1><a href="index.html"><img src="images/logo.png" alt="Salutem"></a></h1>
+                    <nav>
+                        <ul class="sf-menu header_menu">
+                            <li><a href="index.html">About us<strong></strong></a></li>
+                            <li><a href="index-1.html"><span></span>Products &amp; Services<strong></strong></a>
+                                <ul class='submenu'>
+                                    <li><a href="#">Vestibulum iaculis</a></li>
+                                    <li class='sub-menu'><a href="#">Fusce euismod conuat</a>
+                                        <ul class='submenu2'>
+                                            <li><a href="#">Pellentesque sed</a></li>
+                                            <li><a href="#">Aliquam congue ferm</a></li>
+                                            <li><a href="#">Mauris accum</a></li>
+                                        </ul>
+                                    </li>
+                                    <li><a href="#">Pellentesque</a></li>
+                                </ul>
+                            </li>
+
+                            <li><a href="index-2.html">Quote Request<strong></strong></a></li>
+                            <li><a href="index-3.html">Careers<strong></strong></a></li>
+                            <li><a href="index-4.html">Contacts<strong></strong></a></li>
+                            <li><a href="index-5.html">Blog<strong></strong></a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </header> 
+    <div class="div-content">  
+
+
+   
+	
+        <div class="container_12">
+            <div class="grid_12 mar_5px">
+
+                  <div class="marBot4">
+                      <h3>Search result:</h3>
+                      <div id="search-results"></div>
+                  </div>
+            </div>
+        </div>
+
+
+
+
+
+<!--=======footer=================================-->
+              
+
+    </div>
+
+    <footer>
+            <div class="main-footer">
+                <div class="container_12">
+                    <div class="grid_12">
+                        <div class="grid_7 alpha">
+                            <h2 class="h2_footer"><span>Feel free</span> to contact us</h2>
+                            <p>Nemo enim ipsam voluptatem quia volupta aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos quitione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet.</p>
+
+                            <form id="subscribe-form">
+                                <div class="success">Your subscription request has been sent!</div>
+                              <fieldset>
+                                <label class="email">
+                                  <input type="email" value="e-mail address" onfocus="myFocus(this);" onblur="myBlur(this);"/>
+                                  <span class="error">*This is not a valid email address.</span></label>
+                                <div class="btns"><a href="#" class="button more_btn4" data-type="submit">Notify me</a></div>
+                              </fieldset>
+                            </form>
+
+                        </div>
+
+                        <div class="grid_4 omega prefix_1">
+                            <h2 class="h2_footer"><span>Our</span> location</h2>
+                            <address>
+                                <dl>
+                                    <dt>8901 Marmora Road<br>Glasgow, DO4 89GR.</dt>
+                                    <dd><span>Freephone:</span> +1 800 559 6580</dd>
+                                    <dd><span>Telephone:</span> +1 959 603 6035</dd>
+                                    <dd>FAX: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;+1 504 889 9898</dd>
+                                </dl>
+                            </address>
+
+                            <ul class="soc_icons">
+                                <li><a href="#"><i class="icon-google-plus"></i></a></li>
+                                <li><a href="#"><i class="icon-twitter"></i></a></li>
+                                <li><a href="#"><i class="icon-facebook"></i></a></li>
+                                <li><a href="#"><i class="icon-pinterest"></i></a></li>
+                                <li><a href="#"><i class="icon-linkedin"></i></a></li>
+                                <!-- <li><a href="#"><i class="icon-rss"></i></a></li> -->
+                            </ul>
+
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="footer_priv">
+                <div class="container_12">
+                    <div class="grid_12">
+                        <div class="grid_8 alpha">
+                            <nav>
+                                <ul class="sf-menu">
+                                    <li><a href="index.html">About us</a></li>
+                                    <li><a href="index-1.html"><span></span>Products &amp; Services</a></li>
+                                    <li><a href="index-2.html">Quote Request</a></li>
+                                    <li><a href="index-3.html">Careers</a></li>
+                                    <li><a href="index-4.html">Contacts</a></li>
+                                    <li><a href="index-5.html">Blog</a></li>
+                                </ul>
+                            </nav>
+                        </div>
+
+                        <div class="grid_4 omega">
+                            <p><span class="bold">DURABILE</span> &copy; 2013 <a href="index-6.html">Privacy policy</a><br><!--{%FOOTER_LINK} --></p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>  
+        </footer> 
+
+
+  </div>
+<script type="text/javascript">
+ 
+    function myFocus(element){
+           if (element.value == element.defaultValue){
+               element.focus();
+               element.value = '';
+           }
+        }
+        function myBlur(element){
+           if (element.value == ''){
+               element.blur();
+               element.value = element.defaultValue;
+           }
+        }
+</script>					
+</body>
+</html>
